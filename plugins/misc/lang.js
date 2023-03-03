57c341506a7a500d42a6916ee46552f5e2b678bc
Diwaa01Boy <122137999+diwaa01boy@users.noreply.github.com>
2023-03-03 20:13:44 +0530

    Update lang.js

--- plugins/misc/lang.js
+++ plugins/misc/lang.js
@@ -1,6 +1,6 @@
 const {LANGUAGE,VERSION} = require('../../config');
 const {existsSync,readFileSync} = require('fs');
 var json = existsSync(__dirname+'/lang/' + LANGUAGE + '.json') ? JSON.parse(readFileSync(__dirname+'/lang/' + LANGUAGE + '.json')) : JSON.parse(readFileSync(__dirname+'/lang/english.json'));
-console.log("raganork-md "+VERSION)
+console.log("diwaa-md(හැමනියං කාලෙම වතුර ඕනෙ කෝඩ් උස්සන්න එපා😪 "+VERSION)
 function getString(file) { return json['STRINGS'][file]; }
 module.exports = {language: json, getString: getString }
